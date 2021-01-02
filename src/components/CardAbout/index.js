364d31fa45d78046b02f0d2b2d0c474450230a92
Qian Dong <qdong327@gmail.com>
2021-01-02 09:04:17 -0500

    final push

--- src/components/CardAbout/index.js
+++ src/components/CardAbout/index.js
@@ -45,7 +45,8 @@ function CardAbout() {
 					Hello, my name is Quinn Dong-Kilkenny. I am an architectural designer transitioning from the Arch/Engineering/Construction industry into a full-stack web
 					developer. I currently reside in New York, NY and I work at
 					a design firm in Manhattan. I am actively building upon my visualization and design
-					skillset to pursue a pivot into the world of software engineering. I am currently learning: Unity, Processing P5.js, Python, Javascript, and C++. Feel free to reach out via email for collaborations or inquiries.
+					skillset to pursue a pivot into the world of software engineering. I am currently learning: Unity, Processing P5.js, Python, Javascript, and C++. Feel free to reach out via email for collaborations or inquiries. 
+					Check out my <a href="https://www.linkedin.com/in/dong-qian/" target="_blank">LinkedIn profile</a>.
 				</p>
 			</div>
 	);
