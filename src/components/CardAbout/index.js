26887fc56b71677d378a6cb2ece080888265cca5
Qian Dong <qdong327@gmail.com>
2021-01-02 09:05:38 -0500

    cleanup

--- src/components/CardAbout/index.js
+++ src/components/CardAbout/index.js
@@ -12,35 +12,6 @@ function CardAbout() {
 					src={Pic}
 					alt="me"
 				/>
-				<span>
-{/* <form>
-	<label for="inputName">Name</label>
-	<input type="text" class="form-control"></input>
-</form> */}
-
-					{/* <form>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputName">Name</label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Full Name" />
-                            </div>
-                        </div>
-                            <div class="form-group col-md-6">
-                                <label for="inputEmail4">Email</label>
-                                <input type="text" class="form-control" id="inputAddress" placeholder="hello@qdk.com" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Message</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"
-                                placeholder="Let's collab!"></textarea>
-                        </div>
-                        <button type="submit" class="submit btn btn-dark">Submit</button>
-                    </form> */}
-
-
-
-				</span>
 				<p class="text">
 					Hello, my name is Quinn Dong-Kilkenny. I am an architectural designer transitioning from the Arch/Engineering/Construction industry into a full-stack web
 					developer. I currently reside in New York, NY and I work at
