9b02318dc448316a1dc5ad6c2377c01ab6aed278
Qian Dong <qdong327@gmail.com>
2021-01-01 20:25:53 -0500

    test thing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 			<div className="App">
 				<Nav />
 				<Switch>
-					<Route path="/" className="App-link">
+					<Route exact path="/" className="App-link" component={App}>
 						<Header />
 					</Route>
 					<Route exact path="/about" component={About} className="App-link">
